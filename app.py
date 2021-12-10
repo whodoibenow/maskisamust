c31e5d014a81e52ba78efabf524d2c74965af597
Masooque Ali Siddiqui <69395065+whodoibenow@users.noreply.github.com>
2021-12-10 19:46:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def mask_image():
 
 	# load the face mask detector model
     print("Loading face mask detector model...")
-    model = load_model('mask_detection_model.h5')
+    model = load_model('mask_detector-2.model')
 
 	# load the input image from disk, clone it, and grab the image spatial
 	# dimensions
